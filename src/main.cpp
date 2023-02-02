bdab086e4033c69dfa42ceab9d5f4a7d25d16c37
Curtis Hu <curtis.hu688@gmail.com>
2023-02-02 09:33:10 -0800

    prints fine

--- src/main.cpp
+++ src/main.cpp
@@ -14,38 +14,31 @@ uint8_t ADDR[] = {P3_5, P3_6, P7_0, P6_4, P6_3, P6_2, P6_1, P4_1, P4_2, P2_7, P3
 uint8_t DATA[] = {P8_2, P3_7, P4_0, P4_3, P1_2, P1_3, P1_4, P1_5};
 
 void cycle() {
-	int address = 0;
+	unsigned int address = 0;
 	for (int i = 0; i < 16; i++) {
 		int bit = digitalRead(ADDR[i]) ? 1 : 0;
-		address = address<<1 + bit;
-		Serial.print(bit);
+		address = (address<<1) + bit;
 	}
 
-	Serial.print("    ");
-
-	int databus = 0;
+	unsigned int databus = 0;
 	for (int j = 0; j < 8; j++) {
 		int bit = digitalRead(DATA[j]) ? 1 : 0;
-		databus = (databus<<1) + 1;
-		Serial.print(bit);
+
+		databus = (databus<<1) + bit;
 	}
 
 	Serial.print("    ");
 
-	char output[15];
+	char output[40];
 	char read_write = digitalRead(R_W) ? 'r' : 'w';
-	sprintf(output, "%04x   %c   %02x", address, read_write, databus);
+	sprintf(output, "%02x  %02x", address, databus);
 	Serial.println(output);
-}
-
-void print() {
-	Serial.println("PRINT");
 }
 
 void setup()
 {
 	Serial.begin(57600);
-	Serial.print("HELOO WORLD");
+	Serial.println("Initializing...");
 
 	pinMode(CLOCK, INPUT);
 	pinMode(R_W, INPUT);
@@ -54,7 +47,7 @@ void setup()
 	for (int i = 0; i < 8; i++)
 		pinMode(ADDR[i], INPUT);
 
-	attachInterrupt(digitalPinToInterrupt(CLOCK), print, CHANGE);
+	attachInterrupt(digitalPinToInterrupt(CLOCK), cycle, CHANGE);
 }
 
 void loop(){}
